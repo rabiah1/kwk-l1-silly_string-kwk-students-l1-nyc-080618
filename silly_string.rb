84da733f0e615c8fc5665d6cb1209e85451157b7
rabiah1 <rabiahn01@gmail.com>
2018-08-07 14:00:44 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -9,7 +9,7 @@ puts "My favorite word is beautiful"
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS beautiful".validates_length_of
+puts "MY FAVORITE WORD IS beautiful".
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
